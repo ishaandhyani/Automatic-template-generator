58bd1c978e25c5daa457501ba3ef54a81c5710ff
ishaandhyani <53949899+ishaandhyani@users.noreply.github.com>
2022-07-11 15:21:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const { readFileSync, writeFileSync } = require("fs");
+const yaml = require("js-yaml");
+const { v4 } = require("uuid");
+
+const questionObj = {
+  uuid: "uuid",
+  type: "MultipleChoice",
+  content: "question",
+  hint: "hint",
+  explanation: "explanation",
+  answerKeys: ["A"],
+  topic: "01_data_types",
+  sub_topics: ["variables"],
+  difficulty_level: "Low",
+  choices: [
+    {
+      content: "option 1",
+      key: "A",
+      order: 0,
+    },
+    {
+      content: "option 2",
+      key: "B",
+      order: 1,
+    },
+    {
+      content: "option 3",
+      key: "C",
+      order: 2,
+    },
+    {
+      content: "option 4",
+      key: "D",
+      order: 3,
+    },
+  ],
+};
+
+let arrayOfQuestions = [];
+for (let i = 0; i < 50; i++) {
+  let tempObj = questionObj;
+  tempObj.uuid = v4();
+  arrayOfQuestions.push(JSON.parse(JSON.stringify(tempObj)));
+}
+//writeFileSync("emptyFile.json", JSON.stringify(arrayOfQuestions), "utf8");
+//console.log(arrayOfQuestions[7].uuid);
+
+let yamlStr = yaml.dump(arrayOfQuestions);
+writeFileSync("emptyFile.yaml", yamlStr, "utf8");
